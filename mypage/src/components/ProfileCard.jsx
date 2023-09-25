ad548f62b798b6f6489e639222146d83f44c76b5
sing123692 <sing123692@gmail.com>
2023-09-26 06:41:28 +0800

    20230926v1.0

--- mypage/src/components/ProfileCard.jsx
+++ mypage/src/components/ProfileCard.jsx
@@ -27,7 +27,7 @@ const ProfileCard = (props) => {
 
 
                         <div className='avatar'>
-                            <img src="" alt="" />
+                            <img src={require("../images/WelcomPic.png")} alt="" />
                         </div>
 
 
@@ -47,33 +47,55 @@ const ProfileCard = (props) => {
                 </div>
 
 
-                {ShowAboutMe && (
-                    < div className='AboutMe'>
-                        <h1>關於我</h1>
 
-                        <p>
-                            你好！我是曾郁庭，近期剛從資策會畢業，正在尋找網頁前端相關的工作。<br />
-                            我會使用的語言有基本的HTML、CSS、JavaScript，<br />
-                            框架則是使用了React目前正在學習Vue，在此前也有使用過jQuery含式庫，<br />
-                            CSS的部分學習使用了SCSS正在學習TailWindCss<br />
-                            API串接的時候使用AJAX但後來改使用框架的原因改使用了Axios<br />
-                            也會使用Boostrap套用現有的套版。<br />
-                            在學期間，略懂一些後端的程式語言，<br />
-                            目前使用的是MySQL與Express套件去將資料庫與本地伺服器連接。<br />
+                <div className='AboutMe'>
 
-                            <br />
-                        </p>
-                    </div>
-                )}
 
-                {ShowSkill && (
-                    <div className='SkillDiv'>
-                        <h1>前端使用工具</h1>
-                        <p>Figma、HTML、CSS、SCSS、RWD、Bootstraps、JavaScript、JQuery、React.js、GitDesktop</p>
-                        <h1>後端使用工具</h1>
-                        <p>Node.js、Express、MySQL、</p>
-                    </div>
-                )}
+                    {ShowAboutMe && (
+                        <div className='AboutMeText'>
+                            <div className='AboutTitle'>
+                                <h1>關於我</h1>
+
+                            </div>
+
+                            <p>
+                                你好！我是曾郁庭，近期剛從資策會畢業，正在尋找網頁前端相關的工作。<br />
+                                我會使用的語言有基本的HTML、CSS、JavaScript，<br />
+                                框架則是使用了React目前正在學習Vue，在此前也有使用過jQuery含式庫，<br />
+                                CSS的部分學習使用了SCSS正在學習TailWindCss<br />
+                                API串接的時候使用jQuery的AJAX，但後來改使用框架的原因改使用了Axios<br />
+                                也會使用Boostrap套用現有的套版。<br />
+                                在學期間，略懂一些後端的程式語言，<br />
+                                目前使用的是MySQL與Express套件去將資料庫與本地伺服器連接。<br />
+
+                                <br />
+                            </p>
+
+                        </div>
+
+                    )}
+                    {ShowSkill && (
+                        <div className='SkillDiv'>
+                            <div className='SkillCard'> <h1>前端使用工具</h1>
+                                <p>HTML、CSS、RWD、JavaScript、JQuery、</p>
+                                <p>Bootstraps、TailWindCss、SCSS</p>
+                                <p>React.js、Vue.js(略懂)</p>
+
+                            </div>
+                            <div className='SkillCard'><h1>後端使用工具</h1>
+                                <p>Node.js(Express)、MySQL</p>
+                            </div>
+                            <div className='SkillCard'><h1>其他</h1>
+                                <p>Figma、Git、AJAX(axios and jQueryAJAX)</p>
+                            </div>
+
+                        </div>
+                    )}
+
+
+                </div>
+
+
 
 
                 <div className='buttonDiv'>
